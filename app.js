5ef845e4de13ee6abc92f424f03c37ea53011021
HaeMin_MAC <haemin001212@gmail.com>
2020-11-30 21:06:32 +0900

    -Add CORS Enable with Express server.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 const bodyParser = require('body-parser');
 const cUtil = require('./customUtil');
 var cookieParser = require('cookie-parser');
+var cors = require('cors');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -15,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
