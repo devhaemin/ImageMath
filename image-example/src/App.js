d1947864bfd5b5de03700f7e85534f10f77bb35e
HaeMin_MAC <haemin001212@gmail.com>
2020-12-03 19:54:52 +0900

    Add progress for video upload.

--- image-example/src/App.js
+++ image-example/src/App.js
@@ -1,8 +1,19 @@
 import React, {Component} from 'react';
 import { Header } from './component';
 import Main from "./component/main";
+import {progressBarFetch, setOriginalFetch} from "react-fetch-progressbar";
 
 class App extends Component {
+    componentWillMount() {
+        // Let react-fetch-progressbar know what the original fetch is.
+        setOriginalFetch(window.fetch);
+
+        /*
+          Now override the fetch with progressBarFetch, so the ProgressBar
+          knows how many requests are currently active.
+        */
+        window.fetch = progressBarFetch;
+    }
 
     render() {
         return (
